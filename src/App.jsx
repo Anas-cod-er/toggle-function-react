964c5b4b30867880e7dd66e542c82b795038534d
Anas Bin Jaman <anasbinjaman100@gmail.com>
2026-02-06 10:55:31 +0800

    add tailwind daisyui google font

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Daisynav from './component/daisy/Daisynav'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Daisynav></Daisynav>
+      </header>
+
+      <main></main>
+    </>
+  )
+}
+
+export default App
